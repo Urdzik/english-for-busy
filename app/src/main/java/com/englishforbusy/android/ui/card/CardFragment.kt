7d1efe864cc2010f689beb90fa3d56d719eb8271
Viacheslav Urdzik <viacheslav.urdzik@indeema.com>
2021-02-25 02:30:23 +0200

    tts

--- app/src/main/java/com/englishforbusy/android/ui/card/CardFragment.kt
+++ app/src/main/java/com/englishforbusy/android/ui/card/CardFragment.kt
@@ -1,9 +1,11 @@
 package com.englishforbusy.android.ui.card
 
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -11,9 +13,11 @@ import by.kirich1409.viewbindingdelegate.viewBinding
 import com.englishforbusy.android.R
 import com.englishforbusy.android.data.model.Word
 import com.englishforbusy.android.databinding.FragmentCardBinding
+import java.util.*
+
 
 class CardFragment : Fragment() {
-
+    var t1: TextToSpeech? = null
     private val list = listOf(
         Word("ambiguous", "Сумнівний"),
         Word("conventional", "Звичайний"),
@@ -46,6 +50,18 @@ class CardFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        var t1 = TextToSpeech(
+            activity?.applicationContext
+        ) {
+            if(it != TextToSpeech.ERROR) {
+                t1?.language = Locale.US;
+            }
+        }
+
+        binding.imageView8.setOnClickListener {
+            val toSpeak: String = list[index].en
+            t1.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null)
+        }
         binding.imageView7.setOnClickListener {
             findNavController().popBackStack()
         }
@@ -66,12 +82,20 @@ class CardFragment : Fragment() {
         binding.back.setOnClickListener {
             index--
             setData(list[index])
-            binding.progress.setProgress(((index - 1 ) * 10), true)
+            binding.progress.setProgress(((index - 1) * 10), true)
             if (index == 0) {
                 binding.back.visibility = View.GONE
             }
         }
 
+    }
+
+    override fun onPause() {
+        if (t1 != null) {
+            t1!!.stop()
+            t1!!.shutdown()
+        }
+        super.onPause()
     }
 
     private fun setData(data: Word) {
